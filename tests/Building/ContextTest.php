2cfddc7aad054e816ab57c60f5a5e59b8f330cd1
Nicolò Martini <electroportal.net@gmail.com>
2013-08-27 18:03:29 +0200

    Tests for context class

---
+++ tests/Building/ContextTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of library-template
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace Building\Test;
+
+
+use Building\Builder;
+use Building\BuildProcess;
+use Building\Context;
+
+class ContextTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  BuildProcess */
+    protected $process;
+
+    public function setUp()
+    {
+        $this->process = $this->getMock('Building\BuildProcess');
+    }
+
+    public function testConstructor()
+    {
+        $hello = 'hello';
+        $context = new Context($hello, $this->process);
+
+        $this->assertEquals('hello', $context->object);
+        $this->assertEquals($this->process, $context->process);
+    }
+
+    public function testObjectIsPassedByReference()
+    {
+        $hello = 'hello';
+        $context = new Context($hello, $this->process);
+        $context->object = 'bye';
+
+        $this->assertEquals('bye', $context->object);
+    }
+}
+ 
